1a2ecc2930a1312627fa26311b2842f479eeb353
jamesmarkotic <jamesmarkotic@gmail.com>
2018-01-23 23:04:50 -0800

    Initial push

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'pfOlVJXHAKtNtkcGeoQyg6EZ1',
+  consumer_secret: '2E5HBeWx8lKNnz3RAzEHzMPYPGVQYZRMwqD1GjAT6BLXOFNXEl',
+  access_token_key: '955892294837612544-JtNu1ZKTMuuI0qi3D6Cytru67ABMtXJ',
+  access_token_secret: 'Rk4sXFh9P11LNi64JPoEi10JeCLAmjDCOnJiA1VtcfrPH',
+}
+
+module.exports = twitterKeys;
